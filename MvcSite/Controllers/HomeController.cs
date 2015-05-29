1f9c8a840f6153c0fc4f69390212084854aaae2d
Ivan Popov <unfragable@data.bg>
2015-05-29 20:21:42 +0300

    working on email form

--- MvcSite/Controllers/HomeController.cs
+++ MvcSite/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System.Net;
+using System.Net.Mail;
+using System.Web;
 using System.Web.Mvc; 
  
 namespace MvcMovie.Controllers 
@@ -16,6 +18,34 @@ namespace MvcMovie.Controllers
 
         public ActionResult Bio()
         {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            using (MailMessage message = new MailMessage())
+            {
+                message.From = new MailAddress("ivanpop@abv.bg");
+                message.To.Add(new MailAddress("unfragablegaming@gmail.com"));
+                message.Subject = "Message from My Site";
+                message.Body = "Body";
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtpcorp.com";
+                smtp.Port = 2525;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("email", "pass");
+                try
+                {
+                    smtp.Send(message);
+                }
+                catch (SmtpFailedRecipientsException ex)
+                {
+
+                }
+            }
+
             return View();
         }
     } 
