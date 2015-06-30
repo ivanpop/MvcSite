b10c8bbec629838e663c815b737f895de9c47d1f
Ivan Popov <unfragable@data.bg>
2015-06-30 16:37:42 +0300

    30062015
    1. Added language switch

---
+++ MvcSite/Controllers/LanguageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcSite.Controllers
+{
+    public class LanguageController : Controller
+    {
+        //
+        // GET: /Account/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Account/Register
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
